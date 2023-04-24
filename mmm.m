1f4e2471d34ae0ed5dbefd6ab6101f455c0c3ee9
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-04-24 21:06:41 +0200

    Create mmm.m

---
+++ mmm.m
@@ -0,0 +1,12 @@
+>> a=1; b=2; c=3
+%użycie średnika (bez ostatniego polecenia) zwróci poniższy wynik c=
+3
+>> a=1; b=2; c=3;
+%użycie średnika po każdej instrukcji nie wyświetla żadnego wyniku w oknie poleceń,
+% chociaż przypisanie wartości następuje
+>> a=1, b=2, c=3
+%natomiast użycie przecinka po każdej instrukcji daje poniższy wynik
+a=
+1 b=
+2 c=
+3
