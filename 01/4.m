907233dc1ce78858bcc0aa462effd896010106e9
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-08 11:05:07 +0200

    Create 4.m

---
+++ 01/4.m
@@ -0,0 +1,22 @@
+%-----------------------------------------------
+%Modelowanie prostych eksperementow losowych
+%generator liczb losowych - podstawowe defenicje
+%-----------------------------------------------
+  
+%liczba losowa - rezultat jakiegos dzialania; orzel i reszka;
+%licba pseudolosowa
+%generator liczb losowych 
+
+rand 
+%zakres 0 - 1
+%ans = 0.9501
+  
+  
+%rand z zadaniem wartosci
+%rand(m,n)
+rand(3,5)
+% 3 - wiersze, 5 - kolumn; macierz(3x5)
+%ans =
+%    0.1270    0.0975    0.9575    0.9706    0.8003
+%    0.9134    0.2785    0.9649    0.9572    0.1419
+%    0.6324    0.5469    0.1576    0.4854    0.4218
