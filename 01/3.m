715eccca0b4bde2fea98e783130d83f20e1956b8
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-08 10:58:19 +0200

    Create 3.m

---
+++ 01/3.m
@@ -0,0 +1,23 @@
+sin(pi/6)
+cos(pi)
+tan(pi/4)
+sin(5)+cos(5)
+sin(5)^2+ cos(5)^2
+
+% - komientarz 
+a = input ("podaj podstawe")
+b = input ("podaj liczbe logarytmiczna")
+x = log(b)/log(a)
+disp(x)
+  
+%operacje na macierzach
+A = [1 2 5; 3 9 0];
+
+a = [1,2; 3,4]
+b = [1,2]
+c = a*b
+
+%Cwiczenie
+A = [0,2, -10; 7 6 1]
+
+B = [1, 0, -2, 3]
