a7e30a392aa56feb459c025a7f93fb9dc7a84c52
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-08 10:19:53 +0200

    Update 1.m

--- 01/1.m
+++ 01/1.m
@@ -1,1 +1,28 @@
+4 + 3
 
+a = ans + 1
+
+
+%zmienne i stale 
+%ans
+%pl 
+%i,j
+%date 
+%clock
+
+%wybrane funkcje 
+%abs
+%%cos
+%sin
+%log
+
+%6 opieratorow relacji
+% < > <= >= == ~=(rozny od!!!)
+
+
+2 > 3
+% ans = logical 0
+2 < 3
+% ans = logical 1
+x = [1 1 5 5]; y = [0 2 4 6]
+% ans = 0 1 0 1 
