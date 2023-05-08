b1ce3a3ee4db7f9423803a5e116c935aa6bc3031
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-08 10:34:38 +0200

    Create 2.m

---
+++ 2.m
@@ -0,0 +1,12 @@
+%1
+%utworz zmienna x = 2 i sprawdz jej wartosc, a nastepnie oblicz sqrt(x)
+x = 2
+sqrt(x)
+  
+%2
+%przypisz zmiennej y wartosc 4 bez zwracania wyniku obliczen 
+y = 4;
+
+%3 
+%Zapisz w jednym wierszy trzy polecenia a = 1, b = 2, c = 3 z kazdorazowym wypisaniem wartosci 
+a = 1, b = 2, c = 3;
