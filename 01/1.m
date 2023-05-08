1bf8ac46381c83007a0e8cf1899905e8ee518828
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-08 10:27:50 +0200

    Update 1.m

--- 01/1.m
+++ 01/1.m
@@ -26,3 +26,26 @@ a = ans + 1
 % ans = logical 1
 x = [1 1 5 5]; y = [0 2 4 6]
 % ans = 0 1 0 1 
+
+% and & 
+% |
+% ~
+% xor 
+
+
+zmienna = 2 + i
+zmienna = -1 + 2j
+%i j - sa normalizowane do i 
+zmienna = 2
+zmienna = zmienna + 1
+% "=+ - to symbol przypisania
+
+
+&operacje matematyczne
+% 2*2
+% ans/4
+% sqrt(9)
+% log(2)
+% log10(100)
+
+
