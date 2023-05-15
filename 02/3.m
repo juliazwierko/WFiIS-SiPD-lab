e1d881b8fcb8a7243de034344b713093abe43ac1
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-15 17:33:16 +0200

    Update 3.m

--- 02/3.m
+++ 02/3.m
@@ -3,13 +3,13 @@
 %z 0:0.1:10. Przyjmij nast. wartosci: k1 = 3, k2 = 2 i zaloz mozliwosc zmiany ich wartosci w m-pliku.
 %Podpisz osie, umiesc siatke, tytul wykresu i legende
 x = 0:0.1:10
- k1 = 3
-  k2 = 2
-  y1 = x*k1
-  y2 = x.^k2
-  plot(x,y1, x, y2)
-  title('Dwa wykresa)
-        xlabel('x')
-        ylable('y')
-        legend('y1','y2) 
-        grid %siatka
+k1 = 3
+k2 = 2
+y1 = x*k1
+y2 = x.^k2
+plot(x,y1, x, y2)
+title('Dwa wykresa)
+xlabel('x')
+ylable('y')
+legend('y1','y2) 
+grid %siatka
