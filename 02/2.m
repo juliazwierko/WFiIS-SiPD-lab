0bcb183d36d2d8125e111b795725cc7438ac0d6e
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-15 10:14:00 +0200

    Create 2.m

---
+++ 02/2.m
@@ -0,0 +1,7 @@
+%Zadanie
+%przedstaw funkcje sin(x) oraz cos(x) na jednym wykresie dla x w przedziale od -10 do 10
+%Dodatkowo uzyj punktowego rozdzialu dla linii wykresu 
+x = (-10:0.1:10)
+y1 = sin(x)
+y2= cos(x)
+plot(x,y1,':',x, y2, ':')
