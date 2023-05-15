7df434d6b8c2a2cbf3f0bb1c760bed86e2a26ed7
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-15 10:05:17 +0200

    Update 1.m

--- 02/1.m
+++ 02/1.m
@@ -1,1 +1,17 @@
-% 
+%GRAFIKA DWUWYMIAROWA
+x = 0:0.1:10 %zakres od 0 do 10
+ y = x 
+ plot(x,y) %rysowanie wykresa
+ 
+%zmiana kolorow, wykresow mozliwa jest w plot:
+plot(x, y, 'r-') %czerwony wykres
+  plot*x, y, 'r-.') %czerwiny wykres z kropkami 
+%istnieje tabelia roznych waryacje wykresow
+
+x = [1 2 3 4]
+y = [25 0 20 5]
+xlabel('x')
+ylable('y')
+tytle('Wykres')
+  
+  
