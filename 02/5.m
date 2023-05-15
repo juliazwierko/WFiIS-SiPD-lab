4a568fa7b80fc6b494fe438adab9609d6587736e
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-15 11:04:29 +0200

    Create 5.m

---
+++ 02/5.m
@@ -0,0 +1,14 @@
+%Zadanie 
+%1 - Napisz program do obliczenia 3 potegi podanej liczby 
+x = input("Podaj liczbe ")
+y = x.^3
+disp('szescian liczby='),
+disp(x)
+
+%2 - Wykonaj mnozenie i dodawanie na podanych macierzach
+% NA NASTEPNYCH ZAJECIACH!
+% [1 3; 5 9], [4 -7; 10 0]
+a = [1 3; 5 9];
+b = [4 -7; 10 0];
+a + b;
+a * b;
