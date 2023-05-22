a2c3ae6810164082774f5fb377b0bd11ecde2867
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-22 10:23:30 +0200

    Create 2.m

---
+++ 03/2.m
@@ -0,0 +1,32 @@
+%Kombinatoryka i Permutacje 
+%Iloczyn liczb naturalnych 
+factorial(n) 
+  %silnia dowolnej liczby
+
+format long
+f = factorial(22)
+
+ 
+Wypisz wszystkie mozliwe permutacje danego zbioru: [1,2,3];
+wygeneruj je w postaci macierzy 
+perms([1,2,3])
+%ans =
+
+%     3     2     1
+%     3     1     2
+%     2     3     1
+%     2     1     3
+%     1     3     2
+%     1     2     3
+  
+
+%Moda - dana ktora ze znioru pojawia sie najczescie
+%Mediana - wartosc sriodkowa
+mode(x)
+median(x) 
+sort(x)
+  
+x = [1 2 3 4 5 6 7 8 9 10]
+sort(x) - sortowanie w kolejnosci rosnacej 
+mode(x) %ans = 1
+median(x) %ans = 5.5000000
