c72669e955cff0c30049b0162749f1a928db603e
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-22 10:12:51 +0200

    Update 1.m

--- 03/1.m
+++ 03/1.m
@@ -1,1 +1,24 @@
+%Estymacja, prawdopodobienstwo i przydatne wzory 
+%Estymacja 
+%Prawdopodobienstwo 
+
+%binopdf(X, N, P) 
+%ch2pdf(X, V)
+%binocdf(X,N,P)
+  
+%Zadanie:
+%Prawdopodobienstwo wyprudokowania wadliwej sztuki towaru wynosi 
+%p = 0.05. Wybrano losowo próbę o liczebnosci n - 100. Niech xn 
+%bedzie liczba sztuk wadliwych w probce. Oblicz p(xn = 9), 
+%(binocdf)
+%Rozwiazanie:
+1 - binocdf(9,100,0.05)
+  % 0.0282 - odpowiedz
+  
+%Zadanie:
+%Wygeneruj wektor kolumnowy 8 na 1 z rownania rozlozonych losowych 
+%liczb calkowitych z przedzialu [-5,5]. Wykorzystaj funkcje randi
+%Rozwiazanie:
+r = randi([-5,5], 8,1)
+  %8 wierszy, 1 kolumna;
 
