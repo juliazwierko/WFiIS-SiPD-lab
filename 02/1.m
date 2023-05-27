de0e73033e392080bfd39aa61efa45398caf6c96
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-27 14:23:14 +0200

    Update 1.m

--- 02/1.m
+++ 02/1.m
@@ -32,14 +32,14 @@ title('Wykres')
 %subplot
 x = -2*pi:0.1:2*pi
 k1 = 0.5;
- k2 = 1;
+k2 = 1;
 y1 = k1*sin(k2*x)
- y = cos(k2*x)
- subplot(2,2,1)
- plot(x,y1)
+y2 = cos(k2*x)
+subplot(2,2,1)
+plot(x,y1)
  
- subplot(2,2,2)
- plot(x,y2)
+subplot(2,2,2)
+plot(x,y2)
  
 
 
