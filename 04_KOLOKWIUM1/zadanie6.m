2c772251e81449fcb4e8aad588cb1a210738cc2f
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-29 11:54:52 +0200

    Create zadanie6.m

---
+++ 04_KOLOKWIUM1/zadanie6.m
@@ -0,0 +1,7 @@
+a = input("wpisz poczatek przedziala, w jakim chcesz generowac macierz: ")
+b = input("i koniec: ")
+c = 1 %1 wiersz
+d = 5 %5 - ilosc kolumn, bo mamy 5-elementowy zbior wedlug polecenia
+macierz = randi([a,b],c,d)
+disp('wszustkie permutacje podanej macierzy:')
+perms(macierz)
