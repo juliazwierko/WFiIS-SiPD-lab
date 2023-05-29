3e0ca415f41ce162d0459c1071d5f2cab4082b18
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-29 11:55:15 +0200

    Create zadanie7.m

---
+++ 04_KOLOKWIUM1/zadanie7.m
@@ -0,0 +1,5 @@
+disp("-----Program do obliczenia silni !n podanej liczby n-----")
+n = input("Wpisz wartosc n, dla ktorej chcesz obliczyc silnie: ")
+silnia = factorial(n);
+disp("silnia podanej uzytkownikiem licbzy wynosi:")
+disp(silnia)
