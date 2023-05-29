ef24f8165ee6464180ca8e45264eb36afa224cd8
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-29 13:54:47 +0200

    Update zadanie2.m

--- 04_KOLOKWIUM1/zadanie2.m
+++ 04_KOLOKWIUM1/zadanie2.m
@@ -1,6 +1,6 @@
 %obliczenie V stozka z uzyciem skrypta objetnosc.m
 h = input("podaj wysokosc stozka: ")
-r = input("podaj promien stozka ")
+r = input("podaj promien stozka: ")
 a = objetnosc(h,r);
 disp("objetnosc podanego uzytkownikiem stozka jest rowna")
 disp(a)
