a03ed97b71babfcb29673d256222925597d71a70
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-06-12 10:58:05 +0200

    Create 4.m

---
+++ 05/4.m
@@ -0,0 +1,15 @@
+%1
+  %Wykonak potegowanie macierzy
+  %power 
+  A = [4 3 5; 6 7 9]
+  B = [2 3 4; 6 8 7]
+  mnozenie = A .^B;
+  disp(mnozenie)
+
+%2 
+   %wykonaj dzielenie macierzy
+   A = [4 3 5; 6 7 9]
+   B = [2 3 4; 6 8 7]
+   dzielenie = rdivide(A,B)
+   disp(dzielenie)
+    
