a5abf62ebbef2882659941c18eaafe2c6c446e2d
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-06-12 10:16:38 +0200

    Create 1.m

---
+++ 05/1.m
@@ -0,0 +1,40 @@
+%random (dokumentacja MatLab, search space, work space)
+ 
+help rand
+%pomoca w terminale z opisem 
+
+Z = [3, 5, 6; 1, 2, 5]
+%zmienne i typy danych
+Z(1,1) = 6;
+Z(2,2) = 5;
+
+%Matlab - obiekt narzedny 
+% Tablice ->
+%skalar, macierz, wielowymiarowa tablica, tablica pusta
+
+%typy(klasy) danych: 
+%logiczny 
+%tekstowy 
+%numeryczny 
+%tabela 
+%komorka 
+%struktura 
+
+a = 1 
+  %typ numeryczny, double
+  %workspace 
+ 
+%funckja chr
+%tablice znakow, tablice ciagow
+
+  a = 1
+  A = [1 2 3; 4 5 6] 
+  chr = 'uczelnia'
+  
+  chr(1)
+  %dostaniemy litere 'u'
+  
+  chr(1:3)
+  %ucz
+  
+  
