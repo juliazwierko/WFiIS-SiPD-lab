d3982fa5c28f32513b7a3c2f4e2eaa2d5758a007
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-06-19 10:34:22 +0200

    Create 2.m

---
+++ 06/2.m
@@ -0,0 +1,16 @@
+%--------------Live Script--------------
+syms a b c 
+syms f(x)
+f(x) = a*x ^2 + b*x + c 
+
+diff(f, a)
+ 
+%ZADANIE: za pomoca solve() wyznaczyc miejsca zerowe funkcji 
+%solve()
+syms a b c 
+syms f(x)
+f(x) = a*x ^2 + b*x + c 
+m_zerowe = solve(f)
+  %po prawej stronie live script będę zdefinowane zmienne
+
+
