fe263cef6a5fb6c05e900a486b8eb90372b52a69
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-06-26 10:27:25 +0200

    Create 2.m

---
+++ 07/2.m
@@ -0,0 +1,31 @@
+%---------instrukcja warunkowa if---------
+a = input("Wprowadz wartosc dla zmiennej a ")
+b = input("Wprowadz wartosc dla zmiennej b ")
+if(a~=5)
+    a=5;
+    b=b+5;
+end
+disp(["wyswietl zmienna a:", num2str(a)])
+disp(["wyswietl zmienna b:", num2str(b)])
+clear;
+
+%--------------Koniunkcja-------------- 
+a = input("Wprowadz wartosc dla zmiennej a ")
+b = input("Wprowadz wartosc dla zmiennej b ")
+if((a~=5) & (b<10))
+    a=5;
+    b=b+5;
+end
+disp(["wyswietl zmienna a:", num2str(a)])
+disp(["wyswietl zmienna b:", num2str(b)])
+
+%--------------alternatywe--------------
+a = input("Wprowadz wartosc dla zmiennej a ")
+b = input("Wprowadz wartosc dla zmiennej b ")
+if((a~=5) | (b<10))
+    a=5;
+    b=b+5;
+end
+disp(["wyswietl zmienna a:", num2str(a)])
+disp(["wyswietl zmienna b:", num2str(b)])
+
